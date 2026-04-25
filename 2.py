736a53ff588111d18999ac7b4254ce31cb78dd73
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-25 12:18:03 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -6,17 +6,20 @@ load_dotenv()
 
 app = Flask(__name__)
 client = genai.Client()
+history = []
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     answer = None
     if request.method == 'POST':
         p = request.form['prompt']
+        history.append({"role": "user", "parts": [{"text": p}]})
 
         response = client.models.generate_content(
-            model="gemini-3-flash-preview", contents=p
+            model="gemini-3-flash-preview", contents=history
         )
         answer = response.text
+        history.append({"role": "model", "parts": [{"text": answer}]})
 
     return render_template('index.html', answer=answer)
 
