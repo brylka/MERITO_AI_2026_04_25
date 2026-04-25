fe31d2337bd9235e7f3c7b48cef958dc2f740e26
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-25 09:41:11 +0200

    Sobota

---
+++ 1.py
@@ -0,0 +1,11 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Cześć!"
+)
+print(response.text)
