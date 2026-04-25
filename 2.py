fafd6925e5a8ae6af94f65b8b5f5548836c4b438
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-25 11:04:43 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+client = genai.Client()
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
+    answer = None
     if request.method == 'POST':
         p = request.form['prompt']
-        print(f"Użytkownik wpisał: {p}")
-    return render_template('index.html')
+
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview", contents=p
+        )
+        answer = response.text
+
+    return render_template('index.html', answer=answer)
 
 if __name__ == '__main__':
     app.run(debug=True)
