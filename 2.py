64cb8ae09a0d923d79befc5534f5ba4acf287489
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-25 12:38:37 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, request
 from google import genai
 from dotenv import load_dotenv
@@ -6,13 +8,15 @@ load_dotenv()
 
 app = Flask(__name__)
 client = genai.Client()
-history = []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     answer = None
+    history = []
     if request.method == 'POST':
         p = request.form['prompt']
+        history = json.loads(request.form['history'])
         history.append({"role": "user", "parts": [{"text": p}]})
 
         response = client.models.generate_content(
@@ -21,7 +25,8 @@ def hello():
         answer = response.text
         history.append({"role": "model", "parts": [{"text": answer}]})
 
-    return render_template('index.html', answer=answer)
+    return render_template('index.html', answer=answer,
+                           history=history, history_json=json.dumps(history))
 
 if __name__ == '__main__':
     app.run(debug=True)
