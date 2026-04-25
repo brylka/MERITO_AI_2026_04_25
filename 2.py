d41ded1bf6efe3b092c1f3ac3c0a9c5280c8ef3e
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-25 10:32:45 +0200

    Sobota

--- 2.py
+++ 2.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+        p = request.form['prompt']
+        print(f"Użytkownik wpisał: {p}")
     return render_template('index.html')
 
 if __name__ == '__main__':
